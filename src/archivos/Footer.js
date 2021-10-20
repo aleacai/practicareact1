e221e19cba4981fc04c41033a55f95c8bd282516
aleacai <alejandra.cecena17@tectijuana.edu.mx>
2021-10-19 23:00:25 -0700

    upload

---
+++ src/archivos/Footer.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+import '../App.css'
+import PropTypes from 'prop-types';
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div className = "Elfooter">
+                {this.props.textofoo}
+            </div>
+        );
+    }
+}
+
+
+export default Footer;
+
+Footer.propTypes = {
+    textofoo: PropTypes.object
+  
+};
